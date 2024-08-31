2d6e5b0829e19fc29db4401fa859e9cdfb8f3f4c
valentecaio <cvalente.aio@gmail.com>
2024-08-31 17:44:31 -0300

    better cannon aiming

--- scripts/robots/nitro.gd
+++ scripts/robots/nitro.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
+@export var aim_speed := 250
 @export var speed := 250
 @export var acceleration := 3.0
 @export var friction := 2000
+@export var jump_speed := -500
 
 @onready var body_animated_sprite = $BodyAnimatedSprite2D
 @onready var cannon = $Cannon
@@ -11,6 +13,8 @@ extends CharacterBody2D
 @onready var cannon_animated_sprite = $Cannon/CannonRotation/CannonAnimatedSprite2D
 @onready var cannon_animation = $Cannon/CannonAnimationPlayer
 
+var cannon_angle = 0
+var flipped = false
 
 # change velocity with inertia
 func eval_velocity(initial_velocity, input, delta):
@@ -31,50 +35,40 @@ func flip_body_and_cannon(flip):
   else:
     cannon.position = Vector2(2, -8)
     cannon_rotation_node.position = Vector2(11, 4)
-    # cannon.position = Vector(2, -8)
-    # cannon.position = Vector2(13 if flip else -13, -2)
 
-# rotate cannon relative to its parent
-# the left of the cannon is fixed at the origin
 func rotate_cannon(angle):
-  angle = deg_to_rad(angle)
-  cannon.rotation = angle
+  # only acceptable values are -90, -45, 0, 45, 90
+  angle = round(angle / 45) * 45
+  cannon.rotation = deg_to_rad(angle)
 
 
 func _physics_process(delta):
-  # gravity.
+  # gravity
   if not is_on_floor():
     velocity += get_gravity() * delta
 
   var dir = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
+
+  # walk
   if dir.x:
     body_animated_sprite.play("walk")
     cannon_animation.play("cannon_walk")
-    print(body_animated_sprite.frame)
-    flip_body_and_cannon(dir.x < 0)
+    flipped = dir.x < 0
+    flip_body_and_cannon(flipped)
   else:
     body_animated_sprite.pause()
     cannon_animation.pause()
   velocity.x = clamp(eval_velocity(velocity.x, dir.x, delta), -speed, speed)
+
+  # cannon aiming
+  cannon_angle += dir.y * aim_speed * delta
+  cannon_angle = clamp(cannon_angle, -90, 90)
+  rotate_cannon(-cannon_angle if flipped else cannon_angle)
+
+  # jump
+  # if Input.is_action_just_pressed("button_south") and is_on_floor():
+  #   velocity.y = jump_speed
+  #   body_animated_sprite.play("jump")
+
+
   move_and_slide()
-
-  if dir.y > 0:
-    rotate_cannon(45)
-  elif dir.y < 0:
-    rotate_cannon(-45)
-  else:
-    rotate_cannon(0)
-
-  # # Handle jump.
-  # if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-  #   velocity.y = JUMP_VELOCITY
-
-  # # Get the input direction and handle the movement/deceleration.
-  # # As good practice, you should replace UI actions with custom gameplay actions.
-  # var direction = Input.get_axis("ui_left", "ui_right")
-  # if direction:
-  #   velocity.x = direction * SPEED
-  # else:
-  #   velocity.x = move_toward(velocity.x, 0, SPEED)
-
-  # move_and_slide()
