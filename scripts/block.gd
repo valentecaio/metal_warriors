31ac6bf5569d18c2146d33dbfe4d8ab8f9f20a58
valentecaio <cvalente.aio@gmail.com>
2024-09-29 08:03:47 -0300

    add new block types and missing frames for old ones

--- scripts/block.gd
+++ scripts/block.gd
@@ -3,31 +3,13 @@ extends StaticBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 
 # properties defined in the editor
-@export var type: Global.BlockType = Global.BlockType.Red
-@export var hp: int = 75
+@export var type := Global.BlockType.Red
+@export var hp := 75
 
 
 func _ready():
   animated_sprite.pause()
-  match type:
-    Global.BlockType.BrownGray:
-      animated_sprite.animation = "brown_gray"
-    Global.BlockType.BrownGreen:
-      animated_sprite.animation = "brown_green"
-    Global.BlockType.Green:
-      animated_sprite.animation = "green"
-    Global.BlockType.Ice:
-      animated_sprite.animation = "ice"
-    Global.BlockType.Orange:
-      animated_sprite.animation = "orange"
-    Global.BlockType.Red:
-      animated_sprite.animation = "red"
-    Global.BlockType.SpaceStationLeft:
-      animated_sprite.animation = "space_station_left"
-    Global.BlockType.SpaceStationMiddle:
-      animated_sprite.animation = "space_station_middle"
-    Global.BlockType.SpaceStationRight:
-      animated_sprite.animation = "space_station_right"
+  animated_sprite.animation = Global.block_type_names[type]
     
 
 
--- scripts/global.gd
+++ scripts/global.gd
@@ -25,4 +25,37 @@ enum BlockType {
   SpaceStationLeft,
   SpaceStationMiddle,
   SpaceStationRight,
+  EarthStationFloorLeft,
+  EarthStationFloorRight,
+  EarthStationWallLeft,
+  EarthStationWallMiddle,
+  EarthStationWallRight,
+}
+
+# lookup table for block type names
+var block_type_names = {
+  BlockType.BrownGray: "brown_gray",
+  BlockType.BrownGreen: "brown_green",
+  BlockType.Green: "green",
+  BlockType.Ice: "ice",
+  BlockType.Orange: "orange",
+  BlockType.Red: "red",
+  BlockType.SpaceStationLeft: "space_station_left",
+  BlockType.SpaceStationMiddle: "space_station_middle",
+  BlockType.SpaceStationRight: "space_station_right",
+  BlockType.EarthStationFloorLeft: "earth_station_floor_left",
+  BlockType.EarthStationFloorRight: "earth_station_floor_right",
+  BlockType.EarthStationWallLeft: "earth_station_wall_left",
+  BlockType.EarthStationWallMiddle: "earth_station_wall_middle",
+  BlockType.EarthStationWallRight: "earth_station_wall_right",
+}
+
+enum ElevatorType {
+  Wood,
+  Metal,
+}
+
+var elevator_type_names = {
+  ElevatorType.Wood: "wood",
+  ElevatorType.Metal: "metal",
 }
